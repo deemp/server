c44b6dfebf7be387342f62c5c10bd2171b22b223
Danila Danko <br4ch1st0chr0n3@gmail.com>
2024-05-23 11:12:33 +0300

    feat(flake): add flake

---
+++ flake.nix
@@ -0,0 +1,61 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    devshell = {
+      url = "github:deemp/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    systems.url = "github:nix-systems/default";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      imports = [
+        inputs.treefmt-nix.flakeModule
+        inputs.devshell.flakeModule
+      ];
+      perSystem =
+        {
+          self',
+          system,
+          lib,
+          config,
+          pkgs,
+          ...
+        }:
+        {
+          # Auto formatters. This also adds a flake check to ensure that the
+          # source tree was auto formatted.
+          treefmt.config = {
+            projectRootFile = "flake.nix";
+            programs = {
+              nixfmt-rfc-style.enable = true;
+              shellcheck.enable = true;
+            };
+          };
+
+          # Default shell.
+          devshells.default = {
+            commands = {
+              tools = [ pkgs.python3 ];
+            };
+          };
+        };
+    };
+
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+      "https://cache.iog.io"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+    ];
+  };
+}
